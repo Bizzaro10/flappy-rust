9338eb665fb20ebbd4a6150be39a50238bf9704e
Your Name <shibikiran.skv@.com>
2025-07-29 22:51:53 +0530

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,11 @@
+use bevy::prelude::*;
+use plugin::MyPlugin;
+use setup::setup;
+ 
+mod constants;
+mod plugin;
+mod setup;
+ 
+fn main() {
+    App::new().add_systems(Startup, setup).add_plugins(MyPlugin).run();
+}

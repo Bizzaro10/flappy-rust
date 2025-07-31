f4486750d5defb2c85b0ff48d0766836a6c352d4
Kira <128256880+Bizzaro10@users.noreply.github.com>
2025-07-31 22:51:22 +0530

    bug-fix

--- src/ai/mod.rs
+++
@@ -1,7 +0,0 @@
-pub mod neural_network;
-pub mod genetic_algorithm;
-pub mod ai_bird;
-
-pub use neural_network::NeuralNetwork;
-pub use genetic_algorithm::GeneticAlgorithm;
-pub use ai_bird::AIBird;
